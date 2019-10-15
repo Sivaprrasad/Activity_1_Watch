762d1ec581499e656a935bf45443aca8f1f4e39e
mac <mac@notarealemail.com>
2019-10-15 13:06:38 -0400

    PHONE: Update UI so we can debug sending a message to the watch

--- WatchKitDemo-F19/ViewController.swift
+++ WatchKitDemo-F19/ViewController.swift
@@ -14,9 +14,11 @@ class ViewController: UIViewController {
     // ------------------------
     @IBOutlet weak var counterLabel: UILabel!
     
+    @IBOutlet weak var sendMessageOutputLabel: UILabel!
     // MARK: Variables
     // ------------------------
     var phoneCounter:Int = 0
+    var messageCounter:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +35,20 @@ class ViewController: UIViewController {
         self.counterLabel.text = "Counter: \(self.phoneCounter)"
         
     }
+    
+    // When you press button, send a message to watch
+    @IBAction func sendMessageButton(_ sender: Any) {
+        print("Sending message to watch")
+        // ------ SEND MESSAGE TO WATCH CODE GOES HERE
+
+        
+        
+        // -----------------------------------------------
+        messageCounter = messageCounter + 1
+        sendMessageOutputLabel.text = "Message Sent \(messageCounter)"
+    }
+    
+    
     
 }
 
